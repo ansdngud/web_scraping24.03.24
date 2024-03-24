247746f971f7b488d5885948205f532e1e993edb
ansdngud <dngud1013@gmail.com>
2024-03-24 20:51:01 +0900

    pandas 이용해서 추출한 데이터 엑셀에 저장

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
 import requests
+import openpyxl
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # 웹페이지 URL 설정
 url = 'https://www.naver.com/'
@@ -18,6 +20,7 @@ resp = requests.get(url)
 # HTML 파싱 , 웹 페이지의 html 내용을 beatifulSoup 객체로 변환
 bs = BeautifulSoup(resp.text, 'html.parser')
 
+# ----데이터 추출 시작-------
 new_title = bs.find_all('a', class_='sa_text_title')
 
 new_title_list = []
@@ -28,9 +31,16 @@ for title in new_title:
 # 뉴스 제목안에 있던 \n을 제거하는 코드 
 cleaned_news_tit = [item.replace('\n', '') for item in new_title_list]
 
+'''
 for idx, tit in enumerate(cleaned_news_tit):
   print(f"{idx + 1} : {tit}")
+'''
+news_title = []
+for tit in cleaned_news_tit:
+  news_title.append(tit)
   
+print(news_title) 
+''' 
 keyword = '비'
 keyword_tit_list = []
 for tit in cleaned_news_tit:
@@ -38,3 +48,13 @@ for tit in cleaned_news_tit:
     keyword_tit_list.append(tit)
     
 print(keyword_tit_list)
+# ----데이터 추출 끝---
+'''
+# 추출된 데이터를 데이터프레임으로 변환
+data = {'기사제목': news_title}
+
+# 데이터프레임 생성
+df = pd.DataFrame(data)
+
+# 데이터프레임을 엑셀 파일로 저장
+df.to_excel('C:\works\python_project\뉴스_기사.xlsx', index=False)
